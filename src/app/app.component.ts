6484a35e05d2be2be632f9a8c4967624993c4a14
Jeff Peters <petersjeff288@gmail.com>
2020-06-03 22:19:31 -0400

    latest update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'friends-list-ng';
+  name: string = "";
+  age: number = 0;
+  email: string ="";
+  bff: boolean = false;
+  
+  constructor(name:string, age:number, email:string, bff:boolean) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+    this.bff = bff;
+  }
+
+}
+
+let f1: AppComponent = new AppComponent("Jason Herbot", 45, "jason.Herbot@yahoo.com", true);
+let f2: AppComponent = new AppComponent("George Hiben", 57, "George.Hiben@yahoo.com", false);
+let f3: AppComponent = new AppComponent("Mollie Dwyer", 44, "Mollie.Dwyer@yahoo.com", true);
+let f4: AppComponent = new AppComponent("Patrick Dwyer", 49, "null", false);
+let f5: AppComponent = new AppComponent("Jared Johnson", 46, "Jdude@gmail.com", false);
+
+let friends: Array<AppComponent> = [f1, f2, f3, f4, f5];
+
+
+add() {
+  
+  // friends.push(friend);
+  
+
 }
